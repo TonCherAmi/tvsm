77617fb096c808607bab4aad1d3723cc2cfeaf94
Vasili Karaev <kratos661@gmail.com>
2017-01-13 19:59:01 +0300

    Added necessary config entries

--- config.scm
+++ config.scm
@@ -1,10 +1,14 @@
 (define-module (watch config)
   #:export (home-directory
-            resources-directory))
+            resources-directory
+            show-database-path
+            ask-on-existing-show-overwrite?))
 
 (define home-directory (string-append (getenv "HOME") "/"))
 
-(define resources-directory (string-append home-directory ".local/share/watch"))
+;(define resources-directory (string-append home-directory ".local/share/watch"))
+; Development ONLY!
+(define resources-directory (string-append home-directory "code/scheme-projects/watch"))
 
 (define show-database-path (format #f "~a/~a" resources-directory "shows"))
 
