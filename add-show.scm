84f9f2b72c06a7c3cd982de9363ce16cb2903fcd
Vasili Karaev <kratos661@gmail.com>
2017-01-15 20:13:46 +0300

    Moved some generic functions to util.scm, cleaned up imports

--- add-show.scm
+++ add-show.scm
@@ -1,39 +1,29 @@
-(define-module (watch add-show)
-  #:export     (add-show)
-  #:use-module (ice-9 rdelim)
+(define-module  (watch add-show)
+  #:export      (add-show)
+  #:use-module  (ice-9 rdelim)
+  #:use-module  (watch util)
   #:use-module ((watch config)
-                #:prefix config:))
+                  #:prefix config:))
 
 (define* (add-show show-name show-path #:optional (starting-episode 1))
-  (let* ((new-show  (list show-name show-path starting-episode))
+  (let* ((new-show  (create-show show-name show-path starting-episode))
          (show-list (read-show-list))
          (show-list 
            (cond
-             ((and (assoc show-name show-list)
+             ((and (find-show show-name show-list)
                     config:ask-on-existing-show-overwrite?
                    (ask-whether-to-overwrite-show show-name))
-              (cons new-show (assoc-remove! show-list show-name)))
+              (cons new-show (remove-show show-name show-list)))
 ; ----------------------------------------------------------------------------------
-             ((and (assoc show-name show-list)
+             ((and (find-show show-name show-list)
                    (not config:ask-on-existing-show-overwrite?))
-              (cons new-show (assoc-remove! show-list show-name)))
+              (cons new-show (remove-show show-name show-list)))
 ; ----------------------------------------------------------------------------------
-             ((not (assoc show-name show-list))
+             ((not (find-show show-name show-list))
               (cons new-show show-list))
 ; ----------------------------------------------------------------------------------
              (else (throw 'show-already-exists-exception)))))
     (write-show-list show-list)))
-
-(define (read-show-list)
-  (with-input-from-file
-    config:show-database-path
-    read))
-
-(define (write-show-list show-list)
-  (with-output-to-file
-    config:show-database-path
-    (lambda ()
-      (write show-list))))
 
 (define (ask-whether-to-overwrite-show show-name)
   (let loop ((ask-message (format #f 
