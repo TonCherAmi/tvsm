3e5025ac3ab7ff927167cf5845f0d03a4af38307
Vasili Karaev <kratos661@gmail.com>
2017-01-20 07:57:57 +0300

    Added a couple of things

--- add-show.scm
+++ add-show.scm
@@ -5,43 +5,44 @@
   #:use-module ((watch config)
                   #:prefix config:))
 
-;; -------------------------------------------------------------------- ;;
-;; Add show to the show database.                                       ;;
-;; -------------------------------------------------------------------- ;;
-;; #:param: show-name - a string representing the name of the show that ;;
-;;                      is being added,                                 ;;
-;;                      serves as a unique identifier, there cannot     ;;
-;;                      be two shows with the same show-name in the     ;;
-;;                      database                                        ;;
-;;                                                                      ;; 
-;; #:param: show-path - a string that is a path to the directory        ;; 
-;;                      that contains the show                          ;;
-;;                                                                      ;;
-;; #:param: starting-episode - an integer that is the number of the     ;;
-;;                             episode from which the show will         ;; 
-;;                             begin to play, cannot be less than 1     ;;
-;; -------------------------------------------------------------------- ;;
-;; TODO: Add invalid starting episode number handling.                  ;;
-;; -------------------------------------------------------------------- ;;
-(define* (add-show show-name show-path #:optional (starting-episode 1))
-  (let* ((new-show  (create-show show-name show-path starting-episode))
-         (show-list (read-show-list))
-         (show-list 
-           (cond
-             ;; If show with such a name already exists we ask user
-             ;; whether he wishes to overwrite it, if the answer is
-             ;; positive we overwrite it, otherwise we throw an
-             ;; exception.
-             ((and (find-show show-name show-list)
-                   (ask-user-overwrite show-name))
-              (cons new-show (remove-show show-name show-list)))
-             ((not (find-show show-name show-list))
-              (cons new-show show-list))
-             (else (throw 'show-already-exists-exception)))))
-    (write-show-list show-list)))
+;; ---------------------------------------------------------------------- ;;
+;; Add show to the show database.                                         ;;
+;; ---------------------------------------------------------------------- ;;
+;; #:param: show-name - a string representing the name of the show that   ;;
+;;                      is being added,                                   ;;
+;;                      serves as a unique identifier, there cannot       ;;
+;;                      be two shows with the same show-name in the       ;;
+;;                      database                                          ;;
+;;                                                                        ;; 
+;; #:param: show-path - a string that is a path to the directory          ;; 
+;;                      that contains the show                            ;;
+;;                                                                        ;;
+;; #:param: starting-episode - an integer that is the number of the       ;;
+;;                             episode from which the show will           ;; 
+;;                             begin to play, cannot be less than 1       ;;
+;; ---------------------------------------------------------------------- ;;
+(define* (add-show-db show-name show-path #:optional (starting-episode 1))
+  (if (> 1 starting-episode)
+    ;; Throw an exception if starting episode number is less than 1.
+    (throw 'invalid-starting-episode-exception)
+    (let* ((new-show  (create-show show-name show-path starting-episode))
+           (show-list (read-show-list))
+           (show-list 
+             (cond
+               ;; If show with such a name already exists we ask user
+               ;; whether they would like to overwrite it, if the answer 
+               ;; is positive we overwrite it, otherwise we throw an
+               ;; exception.
+               ((and (find-show show-name show-list)
+                     (ask-user-overwrite show-name))
+                (cons new-show (remove-show show-name show-list)))
+               ((not (find-show show-name show-list))
+                (cons new-show show-list))
+               (else (throw 'show-already-exists-exception)))))
+      (write-show-list show-list))))
 
 ;; -------------------------------------------------------------------- ;;
-;; Ask user whether they'd like to overwrite already existing show      ;;
+;; Ask the user whether they'd like to overwrite already existing show  ;;
 ;; -------------------------------------------------------------------- ;;
 ;; #:param: show-name - a string representing the name of the show      ;;
 ;; -------------------------------------------------------------------- ;;
