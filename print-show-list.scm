4034746cda004079db7b94407dbdc3aba9f37503
Vasili Karaev <kratos661@gmail.com>
2017-01-21 19:37:58 +0300

    New module

---
+++ print-show-list.scm
@@ -0,0 +1,15 @@
+(define-module (watch print-show-list)
+  #:export     (print-show-list-db)
+  #:use-module (watch show-utils))
+
+;; ----------------------------------------------------------- ;;
+;; Print contents of show-list database in a neat manner.      ;;
+;; ----------------------------------------------------------- ;;
+;; #:param: verbose - make output more detailed                ;;
+;; ----------------------------------------------------------- ;;
+(define* (print-show-list-db #:optional (verbose #f))
+  (let ((show-list (read-show-list-db)))
+    (if verbose (format #t "total: ~a~%" (length show-list)))
+    (for-each 
+      (lambda (s) (print-show s verbose)) 
+       show-list)))
