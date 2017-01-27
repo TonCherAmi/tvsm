db16651f75bb19974322f9e0c4e20c7dac4b7be4
Vasili Karaev <kratos661@gmail.com>
2017-01-27 19:34:04 +0300

    Now using 'show-current-episode-out-of-bounds?' instead of checking the index manuallly

--- add-show.scm
+++ add-show.scm
@@ -9,37 +9,36 @@
 ;; #:param: new-show-name - a string representing the name of the show    ;;
 ;;                          that is being added,                          ;;
 ;;                          serves as a unique identifier, there cannot   ;;
-;;                          be two shows with the same new-show-name in   ;;
-;;                          the database                                  ;;
+;;                          be two shows with the same name in the db     ;;
 ;;                                                                        ;; 
 ;; #:param: new-show-path - a string that is a path to the directory      ;; 
 ;;                          that contains the show                        ;;
 ;;                                                                        ;;
-;; #:param: starting-episode - an integer that is the number of the       ;;
+;; #:param: starting-episode - an integer that is the index of the        ;;
 ;;                             episode from which the show will           ;; 
-;;                             begin to play, cannot be less than 0       ;;
+;;                             begin to play.                             ;;
 ;; ---------------------------------------------------------------------- ;;
 (define* (add-show-db new-show-name new-show-path #:optional (starting-episode 0))
-  (if (> 0 starting-episode)
-    ;; Throw an exception if starting episode number is less than 0.
-    (throw 'episode-out-of-bounds-exception 
-           "Starting episode number cannot be less than zero.")
-    (let* ((new-show  (make-show new-show-name new-show-path starting-episode))
-           (show-list (read-show-list-db))
-           (show-list 
-             (cond
-               ;; If show with such a name already exists we ask user
-               ;; whether they would like to overwrite it, if the answer 
-               ;; is positive we overwrite it, otherwise we throw an
-               ;; exception.
-               ((and (find-show new-show-name show-list)
-                     (ask-user-overwrite new-show-name))
-                (cons new-show (remove-show new-show-name show-list)))
-               ((not (find-show new-show-name show-list))
-                (cons new-show show-list))
-               (else (throw 'show-already-exists-exception
-                            "Cannot add the show.")))))
-      (write-show-list-db show-list))))
+  (let ((new-show (make-show new-show-name new-show-path starting-episode)))
+    (if (show-current-episode-out-of-bounds? new-show)
+        ;; Throw an exception if starting episode index is out of bounds.
+        (throw 'episode-out-of-bounds-exception 
+               "Starting episode number is out of bounds.")
+        (let* ((show-list (read-show-list-db))
+               (show-list
+                 (cond
+                   ;; If show with such a name already exists we ask user
+                   ;; whether they would like to overwrite it, if the answer 
+                   ;; is positive we overwrite it, otherwise we throw an
+                   ;; exception.
+                   ((and (find-show new-show-name show-list)
+                         (ask-user-overwrite new-show-name))
+                    (cons new-show (remove-show new-show-name show-list)))
+                   ((not (find-show new-show-name show-list))
+                    (cons new-show show-list))
+                   (else (throw 'show-already-exists-exception
+                                "Cannot add the show.")))))
+               (write-show-list-db show-list)))))
 
 ;; -------------------------------------------------------------------- ;;
 ;; Ask the user whether they'd like to overwrite already existing show. ;;
