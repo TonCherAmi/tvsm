8e45ae2fd533966cf3205184bde474206efe86e3
Vasili Karaev <kratos661@gmail.com>
2017-02-01 06:56:22 +0300

    Added 'remove-over-db'

--- remove.scm
+++ remove.scm
@@ -1,5 +1,6 @@
 (define-module (watch remove)
-  #:export     (remove-show-db)
+  #:export     (remove-show-db
+                remove-over-db)
   #:use-module (watch show-utils))
 
 ;; ---------------------------------------------------- ;;
@@ -14,4 +15,16 @@
               (throw 'show-not-found-exception
                      (format #f "cannot remove '~a': No such show" show-name))
               (remove-show show-name show-list-db)))))
+    (write-show-list-db new-show-list)))
+
+;; ---------------------------------------------------- ;;
+;; Remove shows that are over from the database.        ;;
+;; ---------------------------------------------------- ;;
+(define (remove-over-db)
+  (let ((new-show-list
+          (let ((show-list-db (read-show-list-db)))
+            (filter 
+              (lambda (show) 
+                (not (show-over? show)))
+              show-list-db))))
     (write-show-list-db new-show-list)))
