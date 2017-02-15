cd65624c0e7759aa1896ff0101ab0a54b8bf6e7f
Vasili Karaev <kratos661@gmail.com>
2017-02-15 19:14:40 +0300

    Added a couple of procs

--- watch/set.scm
+++ watch/set.scm
@@ -1,8 +1,58 @@
 (define-module (watch set)
-  #:export     (set-current-episode-index-db
+  #:export     (rename-show-db
+                set-show-path-db
+                set-current-episode-index-db
                 jump-to-next-episode-db
                 jump-to-previous-episode-db)
   #:use-module (watch show-utils))
+
+
+;; ------------------------------------------------------ ;;
+;; Rename a show in the db.                               ;;
+;; ------------------------------------------------------ ;;
+;; #:param: old-show-name - a string representing         ;; 
+;;          the name of the show that is being renamed    ;;
+;; #:param: new-show-name - a string representing the new ;;
+;;          name that the show will bear                  ;;
+;; ------------------------------------------------------ ;;
+(define (rename-show-db old-show-name new-show-name)
+  (let ((new-show-list
+          (let* ((show-list-db (read-show-list-db))
+                 (new-show
+                   (let ((show-db (find-show old-show-name show-list-db)))
+                     (if (not show-db)
+                       (throw 'show-not-found-exception
+                              (format #f "cannot rename '~a': No such show" old-show-name))
+                       (make-show new-show-name 
+                                  (show:path show-db)
+                                  (show:current-episode show-db))))))
+            (cons new-show (remove-show old-show-name show-list-db)))))
+    (write-show-list-db new-show-list)))
+
+;; ------------------------------------------------------ ;;
+;; Set a new path for a show.                             ;;
+;; ------------------------------------------------------ ;;
+;; #:param: show-name - a string representing the name of ;;
+;;          the show whose path is being set              ;;
+;; #:param: new-show-path - a string representing the new ;;
+;;          path to the show directory                    ;;
+;; ------------------------------------------------------ ;;
+(define (set-show-path-db show-name new-show-path)
+  (let ((new-show-list
+          (let* ((show-list-db (read-show-list-db))
+                 (new-show
+                   (let ((show-db (find-show show-name show-list-db)))
+                     (if (not show-db)
+                       (throw 'show-not-found-exception
+                              (format #f "cannot set path for '~a': No such show" show-name))
+                       (make-show (show:name show-db)
+                                  new-show-path
+                                  (show:current-episode show-db))))))
+            (if (show:current-episode-out-of-bounds? new-show)
+              (throw 'episode-out-of-bounds-exception
+                     (format #f "cannot set path for '~a': Episode out of bounds" show-name))
+              (cons new-show (remove-show show-name show-list-db))))))
+    (write-show-list-db new-show-list)))
 
 ;; ------------------------------------------------------ ;;
 ;; Set current episode index of show called show-name     ;;
@@ -20,11 +70,13 @@
                    (let ((show-db (find-show show-name show-list-db)))
                      (if (not show-db)
                        (throw 'show-not-found-exception
-                              (format #f "cannot set current episode index for '~a': No such show" show-name))
-                       (make-show (show:name show-db) (show:path show-db) new-index)))))
+                              (format #f "cannot set current episode for '~a': No such show" show-name))
+                       (make-show (show:name show-db)
+                                  (show:path show-db)
+                                  new-index)))))
             (if (show:current-episode-out-of-bounds? new-show)
               (throw 'episode-out-of-bounds-exception
-                     (format #f "cannot set current episode index for '~a': index out of bounds" show-name))
+                     (format #f "cannot set current episode for '~a': Episode out of bounds" show-name))
               (cons new-show (remove-show show-name show-list-db))))))
     (write-show-list-db new-show-list)))
 
@@ -45,7 +97,7 @@
                    (let ((show-db (find-show show-name show-list-db)))
                      (if (not show-db)
                        (throw 'show-not-found-exception
-                              (format #f "cannot jump to next episode for '~a': No Such show" show-name))
+                              (format #f "cannot jump to next episode of '~a': No Such show" show-name))
                        (show:current-episode-inc show-db)))))
             (cons new-show (remove-show show-name show-list-db)))))
     (write-show-list-db new-show-list)))
@@ -67,7 +119,7 @@
                    (let ((show-db (find-show show-name show-list-db)))
                      (if (not show-db)
                        (throw 'show-not-found-exception
-                              (format #f "cannot jump to previous episode for '~a': No Such show" show-name))
+                              (format #f "cannot jump to previous episode of '~a': No Such show" show-name))
                        (show:current-episode-dec show-db)))))
             (cons new-show (remove-show show-name show-list-db)))))
     (write-show-list-db new-show-list)))
