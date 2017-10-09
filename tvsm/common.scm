04396b1ae5da3b28f1aa323c2ab07bf7dbfe2828
Vasili Karaev <kratos661@gmail.com>
2017-10-09 21:24:59 +0300

    init

---
+++ tvsm/common.scm
@@ -0,0 +1,25 @@
+;; tvsm - a tv show manager.
+;; Copyright © 2017 Vasili Karaev
+;;
+;; This file is part of tvsm.
+;;
+;; tvsm is free software: you can redistribute  it and/or modify
+;; it under the terms of the GNU Lesser General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;; 
+;; tvsm is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of 
+;; MERCHENTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+;; GNU Lesser General Public License for more details.
+;;
+;; You should have received a copy of the GNU Lesser General Public License
+;; along with tvsm. If not, see <http://www.gnu.org/licenses/>.
+
+(define-module (tvsm common)
+  #:export     (++))
+
+;; ------------------------------------------------------ ;;
+;; Shorthand for 'string-append'.                         ;;
+;; ------------------------------------------------------ ;;
+(define ++ string-append)
