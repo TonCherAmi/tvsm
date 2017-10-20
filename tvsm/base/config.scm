54230b687329470a70c067455b92db661b44f54c
Vasili Karaev <kratos661@gmail.com>
2017-10-21 01:02:19 +0300

    rework module

--- tvsm/base/config.scm
+++ tvsm/base/config.scm
@@ -18,44 +18,72 @@
 
 (define-module (tvsm base config)
   #:export     (config)
+  #:use-module (srfi srfi-1)
   #:use-module (tvsm common)
   #:use-module (tvsm util path))
 
-;;------------------------------------------------------  ;;
-;; Get a property value from the config.                  ;;
+;;------------------------------------------------------- ;;
+;; Get a property value from the config or throw if it is ;;
+;; not found.                                             ;;
 ;; ------------------------------------------------------ ;;
 ;; #:param: key :: symbol - config property key           ;;
 ;;                                                        ;;
-;; #:return: x :: a - value of property if it is found,   ;;
-;;           #f otherwise                                 ;;
+;; #:return: x :: a - value of property if it is found    ;;
 ;; ------------------------------------------------------ ;;
 (define (config key)
-  (let ((property (assoc key (force *config-list*))))
-    (and=> property cdr)))
+  (let ((property (assoc key (force *config*))))
+    (if property
+      (cdr property)
+      (throw 'config-parameter-missing
+             (format #f "missing config parameter '~a': Invalid config" key)))))
 
 ;; ------------------------------------------------------ ;;
-;; Get a list of possible config locations.               ;;
+;; Get a valid config location or throw if none found.    ;;
 ;; ------------------------------------------------------ ;;
-;; #:return: x :: [string] - list of possible config      ;;
-;;           locations                                    ;;
+;; #:return: x :: string - valid config path              ;;
 ;; ------------------------------------------------------ ;;
-(define (path-list) 
-  (let ((home (++ (getenv "HOME") "/")))
-    (list (++ home ".config/tvsm/config")
-          (++ home ".tvsm")
-          "config")))
+(define (config-path)
+  (let* ((paths (map expand-variables
+                     '("$HOME/.config/tvsm/config"
+                       "$HOME/.tvsm"
+                       "config")))
+         (valid-paths (filter (lambda (path)
+                                (access? path R_OK))
+                              paths)))
+    (if (null? valid-paths)
+      (throw 'config-not-found-exception
+             "cannot continue: Configuration file not found")
+      (car valid-paths))))
+
+;; ------------------------------------------------------ ;;
+;; Read config from a file.                               ;;
+;; ------------------------------------------------------ ;;
+;; #:param: path :: string - path to the config           ;;
+;;                                                        ;;
+;; #:return: x :: [(symbol . a)] - config                 ;;
+;; ------------------------------------------------------ ;;
+(define (read-config path)
+  (catch 'read-error
+    (lambda ()
+      (let ((cfg (with-input-from-file path read)))
+        (if (and (list? cfg) (every pair? cfg))
+          cfg
+          (throw 'read-error))))
+    (lambda (key . args)
+      (throw 'config-malformed-exception
+             "cannot continue: Configuration file malformed"))))
 
 ;; ------------------------------------------------------ ;;
 ;; Expand environment variables in string properties of   ;;
 ;; a config.                                              ;;
 ;; ------------------------------------------------------ ;;
-;; #:param: cfg-lst :: [(symbol . a)] - config list       ;;
+;; #:param: cfg :: [(symbol . a)] - config                ;;
 ;;                                                        ;;
 ;; #:return: x :: [(symbol . a)] - copy of that config    ;;
-;;           list with environment variables in string    ;;
+;;           with environment variables in string         ;;
 ;;           properties expanded                          ;;
 ;; ------------------------------------------------------ ;;
-(define (expand-config cfg-lst)
+(define (expand-config cfg)
   (let ((expand-all (compose expand-user expand-variables)))
     (map (lambda (property)
            (let ((key   (car property))
@@ -64,22 +92,13 @@
                    (if (string? value)
                      (expand-all value)
                      value))))
-         cfg-lst)))
+         cfg)))
 
 ;; ------------------------------------------------------ ;;
 ;; A list containing config properties.                   ;;
 ;; ------------------------------------------------------ ;;
 ;; #:global: *config-list* :: promise [(symbol . a)]      ;;
 ;; ------------------------------------------------------ ;;
-(define *config-list*
+(define *config*
   ;; lazy evaluation prevents uncaught exception on module load
-  (delay
-    (let loop ((paths (path-list)))
-      (cond
-        ((null? paths)
-         (throw 'config-not-found-exception
-                "cannot continue: Configuration file not found"))
-        ((access? (car paths) R_OK)
-         (expand-config (with-input-from-file (car paths) read)))
-        (else
-         (loop (cdr paths)))))))
+  (delay (expand-config (read-config (config-path)))))
